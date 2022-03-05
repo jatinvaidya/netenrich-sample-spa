d2531d3fc528700625a6ca44c17e2641b2766854
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-03-05 18:42:44 +1100

    login route fwd q-params

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useSearchParams } from "react-router-dom";
 
 const Login = () => {
 	const { loginWithRedirect } = useAuth0();
-	loginWithRedirect();
+	let [searchParams, setSearchParams] = useSearchParams();
+	const organization = searchParams.get("organization");
+	const invitation = searchParams.get("invitation");
+	loginWithRedirect({ organization, invitation });
 };
 
 export default Login;
