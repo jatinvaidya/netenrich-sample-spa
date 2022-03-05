bdf6fd5f6b7695b6be436d93671d8d18b9c5ed55
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-03-05 18:19:50 +1100

    login route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from "./views/Profile";
 import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
+import Login from "./components/Login";
 
 // styles
 import "./App.css";
@@ -19,31 +20,32 @@ import initFontAwesome from "./utils/initFontAwesome";
 initFontAwesome();
 
 const App = () => {
-  const { isLoading, error } = useAuth0();
+	const { isLoading, error } = useAuth0();
 
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
+	if (error) {
+		return <div>Oops... {error.message}</div>;
+	}
 
-  if (isLoading) {
-    return <Loading />;
-  }
+	if (isLoading) {
+		return <Loading />;
+	}
 
-  return (
-    <Router history={history}>
-      <div id="app" className="d-flex flex-column h-100">
-        <NavBar />
-        <Container className="flex-grow-1 mt-5">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} />
-          </Switch>
-        </Container>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router history={history}>
+			<div id="app" className="d-flex flex-column h-100">
+				<NavBar />
+				<Container className="flex-grow-1 mt-5">
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/profile" component={Profile} />
+						<Route path="/external-api" component={ExternalApi} />
+						<Route path="/external-api" component={Login} />
+					</Switch>
+				</Container>
+				<Footer />
+			</div>
+		</Router>
+	);
 };
 
 export default App;
